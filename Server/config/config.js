d3e1995a96d2986a7bbdb6e8954590995584809b
hyejin <nhj1043@naver.com>
2020-07-20 12:23:12 +0900

    Feat: git hook 기능 추가했습니다.
    - husky 설치
    - commit 하기 전에 eslint로 검사할 수 있도록 설정
    - eslint에 맞게 변경한 후에 수정한 파일에 대해서 다시 `add` 하여 `commit` 할 수 있어야 함

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,22 +1,21 @@
 module.exports = {
-    // DB 연결 정보를 설정하는 객체
-    test: { // local DB 연결 정보
-      username: 'root',
-      password: process.env.DATABASE_PASSWORD,
-      database: '',
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-      logging: false,
-    },
-    development: {  // 배포할 때 RDS 연결 정보
-      username: 'root',
-      password: process.env.DATABASE_PASSWORD,
-      database: 'weatherdream',
-      host: '',
-      port: 13306,
-      dialect: 'mysql',
-      logging: false,
-    },
-  };
-  
+	// DB 연결 정보를 설정하는 객체
+	test: { // local DB 연결 정보
+			username: 'root',
+			password: process.env.DATABASE_PASSWORD,
+			database: '',
+			host: 'localhost',
+			port: 3306,
+			dialect: 'mysql',
+			logging: false,
+	},
+	development: { // 배포할 때 RDS 연결 정보
+			username: 'root',
+			password: process.env.DATABASE_PASSWORD,
+			database: 'weatherdream',
+			host: '',
+			port: 13306,
+			dialect: 'mysql',
+			logging: false,
+	},
+};

cd17f0ad4ef5f9dc17b377e5e6aec486a8ed1d55
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-07-23 17:38:01 +0900

    Feat: mentor부분 mypage 기능 추가했습니다.
    - profile.js file을 수정했습니다.
    - preOrderedLectures.js file을 수정했습니다.

--- Server/controllers/mentor/mypage/profile.js
+++ Server/controllers/mentor/mypage/profile.js
@@ -1,12 +1,56 @@
+const db = require('../../../models');
+
+const { Mentors } = db;
+
 module.exports = {
     get: (req, res) => {
-        console.log('GET mypage/profile');
-        console.log(req.params);
-        res.json({ message: 'ok!' });
+        if (req.params.id) {
+            Mentors
+            .findOne({
+                where: {
+                    id: req.params.id,
+                },
+            })
+            .then((result) => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(409).send('Wrong Access');
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+        }
     },
     post: (req, res) => {
-        console.log('POST mypage/profile');
-        console.log(req.params);
-        res.json({ message: 'ok!' });
+        const {
+            username, nickname, sex, birthday, phone, intro,
+        } = req.body;
+        Mentors
+        .findOne({
+            where: {
+                id: req.params.id,
+            },
+            defaults: {
+                username,
+                nickname,
+                sex,
+                birthday,
+                phone,
+                intro,
+                null: null,
+            },
+        })
+        .then((result) => {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(409).send('Wrong Access');
+            }
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
     },
 };

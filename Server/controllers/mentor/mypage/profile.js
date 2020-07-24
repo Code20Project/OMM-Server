f1bfc1d5ff0bf4e081477f475665781ca3e0a8d5
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-07-24 13:51:59 +0900

    test: commit test

--- Server/controllers/mentor/mypage/profile.js
+++ Server/controllers/mentor/mypage/profile.js
@@ -1,12 +1,57 @@
+const db = require('../../../models');
+
+const { Mentors } = db;
+
 module.exports = {
     get: (req, res) => {
-        console.log('GET mypage/profile');
-        console.log(req.params);
-        res.json({ message: 'ok!' });
+        if (req.params.id) {
+            Mentors
+            .findOne({
+                where: {
+                    id: req.params.id,
+                },
+            })
+            .then((result) => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(409).send('Wrong Access');
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+        }
     },
     post: (req, res) => {
-        console.log('POST mypage/profile');
-        console.log(req.params);
-        res.json({ message: 'ok!' });
+        const {
+            nickname, birthday, phone, intro,
+        } = req.body;
+        Mentors
+        .findOne({
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((result) => {
+            if (result) {
+                Mentors.update({
+                    nickname,
+                    birthday,
+                    phone,
+                    intro,
+                }, { where: { id: req.params.id } })
+                .then(() => res.status(200).json({ message: 'ok!' })).catch((err) => {
+                    console.log(err);
+                });
+            } else {
+                res.status(409).json({
+                    message: 'Wrong Access',
+                  });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
     },
 };

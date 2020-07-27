033141d5691c5928fbfedc68b3fd2dbef275fbff
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-07-27 15:22:53 +0900

    fix: preOrderedLectures에서 lecture를 Reservation으로 수정했습니다.
    - preOrderedLectures.js file을 수정했습니다.

--- Server/controllers/mentee/mypage/preOrderedLectures.js
+++ Server/controllers/mentee/mypage/preOrderedLectures.js
@@ -1,7 +1,26 @@
+const db = require('../../../models');
+
+const { Reservation } = db;
+
 module.exports = {
     get: (req, res) => {
-        console.log('GET mypage/pre-ordered_Lecture');
-        console.log(req.params);
-        res.json({ message: 'ok!' });
+        if (req.params.id) {
+            Reservation
+            .findOne({
+                where: {
+                    id: req.params.id,
+                },
+            })
+            .then((result) => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(409).json({ message: 'Wrong Access' });
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+        }
     },
 };

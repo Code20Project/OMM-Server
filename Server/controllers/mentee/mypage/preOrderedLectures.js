4bc075eae831bd1bb2c4e626bd83309480ddedfc
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-07-28 11:11:16 +0900

    fix: preOrderedLectures에 대한 기능을 수정했습니다.
    - preOrderedLectures.js file을 수정했습니다.

--- Server/controllers/mentee/mypage/preOrderedLectures.js
+++ Server/controllers/mentee/mypage/preOrderedLectures.js
@@ -1,12 +1,18 @@
 const db = require('../../../models');
 
 const { Reservation } = db;
+const { LectureReservation } = db;
 
 module.exports = {
     get: (req, res) => {
         if (req.params.id) {
             Reservation
             .findOne({
+                include: [
+                    {
+                        model: LectureReservation,
+                    },
+                ],
                 where: {
                     id: req.params.id,
                 },
